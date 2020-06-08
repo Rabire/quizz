7454abcc05c0175ebaa26a8efeeb5d20808138a5
Rabire <rabireh@outlook.fr>
2020-06-08 14:05:54 +0200

    structure and begining

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import './App.css'
+
+import QuestionsData from './datas/QuestionsData.json';
+
+import Question from './components/Question'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    console.log(QuestionsData)
+
+    return (
+        <div className="app">
+            <h1>Mon QUIZZ</h1>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/Question.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './Question.css'
+
+function Question() {
+
+
+    return(
+        <div className="question">
+
+        </div>
+    )
+}
+
+export default Question
